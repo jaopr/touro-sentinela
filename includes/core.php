59ff99959a240c45ed5978ffa4d1989715162109
João Paulo <joaopelite25@gmail.com>
2025-06-05 11:05:12 -0300

    Adicionando arquivos de comparação

--- includes/core.php
+++ includes/core.php
@@ -6,13 +6,13 @@ add_action('admin_menu', 'touro_sentinela_adicionar_menu');
 
 function touro_sentinela_adicionar_menu() {
     add_menu_page(
-        'Touro Sentinela',           // Título da página
-        'Touro Sentinela',           // Nome no menu
-        'manage_options',            // Permissão necessária
-        'touro-sentinela',           // Slug da página
-        'touro_sentinela_pagina_html', // Função que exibe o conteúdo
-        'dashicons-shield-alt',      // Ícone do menu (ícones: https://developer.wordpress.org/resource/dashicons/)
-        56                           // Posição no menu
+        'Touro Sentinela',           
+        'Touro Sentinela',           
+        'manage_options',            
+        'touro-sentinela',           
+        'touro_sentinela_pagina_html', 
+        'dashicons-shield-alt',      
+        56                           
     );
 }
 
